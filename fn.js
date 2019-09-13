42bbb6a954ecce72bdce35453149c46edd7a20d9
ccampbell1197 <54957978+ccampbell1197@users.noreply.github.com>
2019-09-13 13:53:19 -0500

    Add files via upload

---
+++ fn.js
@@ -0,0 +1,7 @@
+var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
+floatAge = parseFloat(Prompt("Enter your age in years"));
+floatDays = floatAge*365.25;
+floatMonths = floatAge*12
+intWeeks = floatDays/7
+intFortnights = floatDays/14
+alert(floatAge, floatDays, floatMonths, intFortnights, intWeeks)
