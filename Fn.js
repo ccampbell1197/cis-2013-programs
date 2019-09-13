aff085f6aee9cd0d948c2f5c7d1dca88a44e6c3a
ccampbell1197 <54957978+ccampbell1197@users.noreply.github.com>
2019-09-13 14:03:18 -0500

    Update Fn.js

--- Fn.js
+++ Fn.js
@@ -1,10 +1,10 @@
 var floatAge, floatDays, intWeeks, floatMonths, intFortnights;
 floatAge = parseFloat(prompt("Enter your age in years"));
 floatDays = floatAge*365.25;
-floatMonths = floatAge*12
-intWeeks = floatDays/7
-intFortnights = floatDays/14
-alert(floatAge)
-alert(floatDays)
-alert(intWeeks)
-alert(intFortnights)
+floatMonths = floatAge*12;
+intWeeks = floatDays/7;
+intFortnights = floatDays/14;
+alert(floatAge);
+alert(floatDays);
+alert(intWeeks);
+alert(intFortnights);
