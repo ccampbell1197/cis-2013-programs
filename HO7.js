5a79160881340e02b0a2e4818b67753457c39af1
ccampbell1197 <54957978+ccampbell1197@users.noreply.github.com>
2019-10-28 10:56:57 -0500

    Update HO7.js

--- HO7.js
+++ HO7.js
@@ -54,19 +54,30 @@ var calcCircleCircum = function()
 	alert ("The Circle circumference is " + circleCircum(floatRadius));	
 };
 
-/*
- * the circleCircum function accepts some input for radius and then calculates the circumference
- * using the 2 * pi * radius formula.  The circumference value is returned, but note that it has been
- * converted to a string with 2 decimal points [the toFixed() method in JavaScript takes a number and 
- * lops off everything after the desired decimal points, and then for some unexplicable reason converts
- * the resulting number into a string]
- */
+
 function circleCircum(floatRadius_par)
 {
 	var floatCircum = parseFloat (2 * Math.PI * floatRadius_par);
 	return floatCircum.toFixed(2);
 }
+//CIRCLE AREA
 
+
+var calcCircleArea = function()
+{
+	var floatRadius = parseFloat($("radius").value);
+
+	alert ("The Circle circumference is " + circleArea(floatRadius));	
+};
+
+
+function circleArea(floatRadius)
+{
+	var floatArea = parseFloat (Math.PI * floatRadius^2);
+	return floatArea.toFixed(2);
+}
+
+//
 function rectArea(floatRectHeight,floatRectWidth)
 {
     var floatRectArea = parseFloat (floatRectHeight * floatRectWidth);
@@ -108,6 +119,20 @@ var calcTriangleArea = function()
     alert ("The triangle area is" + triangleArea(floatTriSide1,floatTriSide2,floatTriSide3));
 };
 
+function trianglePerim(floatTriSide1,floatTriSide2,floatTriSide3)
+{
+    floatTrianglePerim = (floatTriSide1+floatTriSide2+floatTriSide3);
+    return floatTrianglePerim.toFixed(2);
+}
+
+var calcTrianglePerim = function()
+{
+    var floatTriSide1 = parseFloat($("tri_side1").value);
+	var floatTriSide2 = parseFloat($("tri_side2").value);	
+	var floatTriSide3 = parseFloat($("tri_side3").value);
+    alert ("The triangle area is" + trianglePerim(floatTriSide1,floatTriSide2,floatTriSide3));
+};
+
 
 
 
@@ -122,8 +147,10 @@ window.onload = function ()
 //    $("radius").value = "";
     $("rect_width").focus(); // places the cursor in the first input box
     $("calc_all").onclick = do_it; // runs the do_it function when clicked
-    $("calc_circle_circum").onclick = calcCircleCircum; // runs the calcCircleCircum function when clicked
+    $("calc_circle_circum").onclick = calcCircleCircum;
+    $("calc_circle_area").onclick = calcCircleArea;
     $("calc_rect_area").onclick = calcRectArea;
     $("calc_rect_perim").onclick = calcRectPerim;
     $("calc_tri_area").onclick = calcTriangleArea;
+    $("calc_tri_perim").onclick = calcTrianglePerim;
 };
