b307a745c814b6f9c119f4d5eb2268ffe1bdad3b
ccampbell1197 <54957978+ccampbell1197@users.noreply.github.com>
2019-11-22 11:34:13 -0600

    Update HO9.js

--- HO9.js
+++ HO9.js
@@ -4,16 +4,14 @@ var $ = function (id)
     return document.getElementById(id);
 }
 
-// Main program function that accepts user input for the total number of the
-//Fibonacci series to show and the then runs a loop to create the output
 
 var generate = function () 
 {
-
-	var i=0;  // sets the first number in the Fibonacci series to 0
-	var j=1;  //sets the second number in the Fibonacci series to 1
-	var k;    //k is calculated by adding i+j and represents the next Fibonacci number
-	var stringOutput = i +" " + j ;//initialize the Fibonacci series output to include the first two numbers
+var i,j,k,stringOutput;
+	 i=0;  // sets the first number in the Fibonacci series to 0
+	 j=1;  //sets the second number in the Fibonacci series to 1
+	    //k is calculated by adding i+j and represents the next Fibonacci number
+ stringOutput = i +" "j ;//initialize the Fibonacci series output to include the first two numbers
 	var intCount = parseInt($("total_fib").value);  //gets user input from the DOM
 	intCount = 2;
 	//validate user input that intCount is a number and intCount is not less than 2 or intCount not greater than 100
@@ -29,13 +27,18 @@ var generate = function ()
         k=i+j;
         i=j;
         j=k;
-        intCount = 2 ;
-        stringOutput = stringOutput + " " + k;
+        stringOutput = stringOutput + k;
+        intCount--;
       }
-      $("output").value = stringOutput;
+    $("output").value = stringOutput;
+    alert(stringOutput);
     }
-
+//While loop and alert provide the requested out put quantity of f numbers
 }
+
+i=0;
+j=1;
+//resetting i and j
 // Push solution back to Output through DOM
 
 window.onload = function () 
